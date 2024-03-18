d1401550aade44f29db39e003e8093175eddcedc
lppduy <lephamphuongduy@gmail.com>
2024-03-18 11:57:24 +0700

    Optimize movie controller by abstracting common logic into getMovies function

--- BackEnd/controllers/movie.js
+++ BackEnd/controllers/movie.js
@@ -1,9 +1,11 @@
 const Movies = require('../models/Movies');
 const paginate = require('../utils/paging').paginate;
 
+const PAGE_SIZE = 20;
+
+// helper function
 const getMovies = (sortField) => (req, res) => {
   const page = parseInt(req.query.page) || 1;
-  const PAGE_SIZE = 20;
   const movies = Movies.all().sort((a, b) => b[sortField] - a[sortField]);
   const paginatedMovies = paginate(movies, page, PAGE_SIZE);
 
@@ -14,10 +16,5 @@ const getMovies = (sortField) => (req, res) => {
   });
 };
 
-const getTrendingMovies = getMovies('popularity');
-const getTopRatedMovies = getMovies('vote_average');
-
-module.exports = {
-  getTrendingMovies,
-  getTopRatedMovies,
-};
+exports.getTrendingMovies = getMovies('popularity');
+exports.getTopRatedMovies = getMovies('vote_average');
